b26a858b843d6f26e1884d635a2177c7d46f27a0
qwerty250903 <107502219+qwerty250903@users.noreply.github.com>
2023-08-22 12:59:34 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
   require_once 'vendor/autoload.php';
+  require_once 'getdata.php';
+  require_once 'postdata.php';
   
   print "Processing...\n";
   class DevClass { 
@@ -12,7 +14,6 @@
       $result = $this->getMembers($access_token,$group_id, $count);
       return $result;
     }
-    
     //Получиаем данные
     public function getMembers($access_token, $group_id, $count) {
       print "\nВыполняетя:\n\nAPI: ". $access_token . "\n\n";  
@@ -26,56 +27,17 @@
       //запрос на получения количества участников
       $count = getMethod("execute", ["code" => urlencode($code), "v"=>"5.131", "access_token" => $access_token], true);
       $count = $count->response;
-      
       return $count;
     }
   }
+
+
   $group_id = readline("Введите айди группы: ");
   $access_token = readline("Введите API токен: ");
   $dev = new DevClass();
   $res = $dev->devMethod($access_token, $group_id);
   $count = $dev->countMembers($access_token, $group_id);
   postMethod($res, $count);
- 
-  function getMethod($method, $options = [], $decode = true){
-    $query = "?".http_build_query($options);
-    $url = "https://api.vk.com/method/".$method.urldecode($query);
-    $data = file_get_contents($url);
-    $decoded = json_decode($data);
-    return ($decode ? $decoded : $data);
-  }
 
-  function postMethod($res, $count){ 
-    $client = new MongoDB\Client("mongodb://localhost:27017");
-    $collection = $client->vk_api->GroupMembers;
-    for ($i=0; $i < $count; $i++) {
-      print_r($res->response[$i]);
-      $first_name = $res->response[$i]->first_name;
-      $last_name = $res->response[$i]->last_name;
-      if(!empty($res->response[$i]->bdate)){
-        $birthday = $res->response[$i]->bdate;
-        $age = date_diff(date_create($birthday), date_create('now'))->y;
-        //print($age);
-      }
-      else{
-        $birthday = 'NULL';
-        $age = 0;
-      }
-      if(!empty($res->response[$i]->country)){
-        $country = $res->response[$i]->country->title;
-      }
-      else{
-        $country = 'NULL';
-      }
-      if(!empty($res->response[$i]->city)){
-        $city = $res->response[$i]->city->title;
-      }
-      else{
-        $city = 'NULL';
-      }
-      $collection->insertOne(['first_name' => $first_name, 'last_name'=> $last_name, 'bdate'=> $birthday, 'age'=>$age, 'country'=>$country,'city'=>$city]);
-      //print_r("\nInsersted with Object ID". $result->getInsertID());
-    }
-    print"\n\nКоличество участников: ". $count ."\n\n";
-  }
+  
 ?>
