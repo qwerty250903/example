024522ef0fcb9796954d60fd92066abd73da1a27
qwerty250903 <107502219+qwerty250903@users.noreply.github.com>
2023-08-22 13:39:31 +0500

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,15 @@
   class DevClass { 
     //Получиаем количество пользователей
     public function devMethod($access_token, $group_id){
-      $code = 'var members = API.groups.getMembers({"group_id": '. $group_id.', "v": "5.131", "sort": "id_asc", "offset": 0, fields: "name, birthday, country, city, age"}).items;var offset = 1000;while (offset < 25000 && (offset + 0) < 100){members = members + "," + API.groups.getMembers({"group_id": '. $group_id.', "v": "5.131", "sort": "id_asc", "count": 1000, "offset": (0 + offset)}).items;offset = offset + 1000;};return API.groups.getMembers({"group_id": '. $group_id.', "v": "5.131", "sort": "id_asc", "offset": 0, fields: "name, birthday, country, city, age"}).count;';
+
+      $code = 'var members = API.groups.getMembers({"group_id": '. $group_id.', 
+        "v": "5.131", "sort": "id_asc", "offset": 0, fields: "name, birthday, country, city, age"}).items;
+        var offset = 1000;while (offset < 25000 && (offset + 0) < 100)
+        {members = members + "," + API.groups.getMembers({"group_id": '. $group_id.', 
+        "v": "5.131", "sort": "id_asc", "count": 1000, "offset": (0 + offset)}).items;offset = offset + 1000;};
+        return API.groups.getMembers({"group_id": '. $group_id.', 
+        "v": "5.131", "sort": "id_asc", "offset": 0, fields: "name, birthday, country, city, age"}).count;';
+
       //запрос на получения количества участников
       $count = getMethod("execute", ["code" => urlencode($code), "v"=>"5.131", "access_token" => $access_token], true);
       $count = $count->response;
@@ -17,13 +25,22 @@
     //Получиаем данные
     public function getMembers($access_token, $group_id, $count) {
       print "\nВыполняетя:\n\nAPI: ". $access_token . "\n\n";  
-      $code = 'return API.groups.getMembers({"group_id": '. $group_id.', "v": "5.131", "sort": "id_asc", "count":'. $count .', "offset":'. 0 .', fields: "name, bdate, country, city, age"}).items;';
+      $code = 'return API.groups.getMembers({"group_id": '. $group_id.', 
+      "v": "5.131", "sort": "id_asc", "count":'. $count .', "offset":'. 0 .', fields: "name, bdate, country, city, age"}).items;';
       //запрос на получения данных n-нного количества пользователей
       $answer = getMethod("execute", ["code" => urlencode($code), "v"=>"5.131", "access_token" => $access_token], true);
       return $answer;
     }
     public function countMembers($access_token, $group_id){
-      $code = 'var members = API.groups.getMembers({"group_id": '. $group_id.', "v": "5.131", "sort": "id_asc", "offset": 0, fields: "name, birthday, country, city, age"}).items;var offset = 1000;while (offset < 25000 && (offset + 0) < 100){members = members + "," + API.groups.getMembers({"group_id": '.$group_id.', "v": "5.131", "sort": "id_asc", "count": 1000, "offset": (0 + offset)}).items;offset = offset + 1000;};return API.groups.getMembers({"group_id": '. $group_id.', "v": "5.131", "sort": "id_asc", "offset": 0, fields: "name, birthday, country, city, age"}).count;';
+
+      $code = 'var members = API.groups.getMembers({"group_id": '. $group_id.', 
+        "v": "5.131", "sort": "id_asc", "offset": 0, fields: "name, birthday, country, city, age"}).items;
+        var offset = 1000;while (offset < 25000 && (offset + 0) < 100)
+        {members = members + "," + API.groups.getMembers({"group_id": '.$group_id.', 
+        "v": "5.131", "sort": "id_asc", "count": 1000, "offset": (0 + offset)}).items;offset = offset + 1000;};
+        return API.groups.getMembers({"group_id": '. $group_id.', 
+        "v": "5.131", "sort": "id_asc", "offset": 0, fields: "name, birthday, country, city, age"}).count;';
+      
       //запрос на получения количества участников
       $count = getMethod("execute", ["code" => urlencode($code), "v"=>"5.131", "access_token" => $access_token], true);
       $count = $count->response;
@@ -39,5 +56,3 @@
   $count = $dev->countMembers($access_token, $group_id);
   postMethod($res, $count);
 
-  
-?>
