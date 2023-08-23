1ad08e8bb5a347de82d70448b6f809c956a16291
qwerty250903 <107502219+qwerty250903@users.noreply.github.com>
2023-08-23 14:12:06 +0500

    Delete getdata.php

--- getdata.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once 'vendor/autoload.php';
-    function getMethod($method, $options = [], $decode = true){
-        $query = "?".http_build_query($options);
-        $url = "https://api.vk.com/method/".$method.urldecode($query);
-        $data = file_get_contents($url);
-        $decoded = json_decode($data);
-        return ($decode ? $decoded : $data);
-    }
