425e0780834695b1d03b0057f37f7e63f2bf29c0
qwerty250903 <107502219+qwerty250903@users.noreply.github.com>
2023-08-23 14:13:18 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,20 @@
 <?php
-  namespace Example;
-
-  require_once __DIR__ . '/vendor/autoload.php'; //зачем __DIR__ есть можно просто require_once 'vendor/autoload.php'
-  require_once 'getdata.php';
-  require_once 'postdata.php';
-
-  use Example\DevCLass\DevClass;
+  
+  require_once __DIR__ . '/vendor/autoload.php'; //зачем __DIR__ есть можно просто require_once 'vendor/autoload.php';
+  use Example\DevCLass\GetPublicMembers;
+  use Example\DevCLass\SaveMembers;
   
   print "Processing...\n";
-
+  //получение данных от пользователя
   $group_id = readline("Введите айди группы: ");
   $access_token = readline("Введите API токен: ");
-  $dev = new DevClass();
-  $res = $dev->devMethod($access_token, $group_id);
-  $count = $dev->countMembers($access_token, $group_id);
-  postMethod($res, $count);
+  //получение пользователей сообщества
+  $members = new GetPublicMembers();
+  //получение информации пользователей
+  $result = $members->getMembersInfo($access_token, $group_id);
+  //получение количества пользователей
+  $count = $members->getMembersCount($access_token, $group_id);
+  //сохранение данных
+  $save = new SaveMembers;
+  $save->saveMembers($res, $count);
 
